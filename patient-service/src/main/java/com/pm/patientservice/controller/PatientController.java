1ed17b31acb8fa5e0d3e56284896082391d8d5d4
Rohitdrk22 <drkrohit22@gmail.com>
2026-03-25 00:06:18 +0530

    Adding the (api-gateway) for the project

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:5173") // Vite port
 @RestController
 @RequestMapping("/patients")
 @Tag(name = "Patient", description = "API for managing Patients")
