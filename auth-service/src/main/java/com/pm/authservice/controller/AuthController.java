83eebef7ba8b9a438e004c375c38ed0c014a75de
Rohitdrk22 <drkrohit22@gmail.com>
2026-03-30 17:23:18 +0530

    Working on controller -> AuthController

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Operation(summary = "Generate token on use login")
+    @PostMapping("login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO){
+
+
+        return null;
+    }
+
+}
